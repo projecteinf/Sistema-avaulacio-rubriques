9b5da090d08a9bc9832b4fde9a47abed13c06b99
Miquel Boada <projecteinf@boscdelacoma.cat>
2022-06-25 10:59:17 -0400

    Afegit script actualització git

---
+++ git.sh
@@ -0,0 +1,19 @@
+git add -A
+
+if [ $# -eq 0 ]
+then
+	echo "És obligatori introduir el comentari"
+	echo './git.sh "Comentari modificació"'
+	exit 1
+fi
+
+if [ $# -gt 1 ]
+then
+	echo "Només és permès un comentari"
+	echo './git.sh "Comentari modificació"'
+	exit 1
+fi
+
+
+git commit -m "$1"
+#git push origin main
