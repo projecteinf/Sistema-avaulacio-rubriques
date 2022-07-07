f5d109860ede9362353b8773cd92b11e4019d998
ProjecteInf <projecteinf@boscdelacoma.cat>
2022-07-07 13:37:01 -0400

    Estructura base aplicació

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './projecte/components/header/header.component';
-import { RowComponent } from './projecte/components/row/row.component';
-import { CellComponent } from './projecte/components/cell/cell.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    RowComponent,
-    CellComponent
   ],
   imports: [
     BrowserModule, FormsModule
