06cff9531fec58e9117cdca84d3f283fadfa5def
ProjecteInf <projecteinf@boscdelacoma.cat>
2022-07-26 10:14:49 +0000

    Guardar token també a memòria

--- src/app/projecte/_model/01-serviceLayer/managers/serviceManager.ts
+++ src/app/projecte/_model/01-serviceLayer/managers/serviceManager.ts
@@ -1,7 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IPersistenceManager } from '../../03-persistenceLayer/managers/iPersistenceManager';
-import { PersistenceTechnologies } from '../../04-utilitiesLayer/appUtilities';
 import { IServiceManager } from './iServiceManager';
 
 @Injectable({
@@ -13,8 +11,6 @@ export class ServiceManager implements IServiceManager{
 
     constructor(){
         this.persistenceManagers=[];
-       
-
     }
 
 }     
