adbbf28177aa52339cdf4be3ff7009d813649aa2
ProjecteInf <projecteinf@boscdelacoma.cat>
2022-08-06 09:07:20 +0000

    Afegida ruta per component AppComponent. Configuració no funcional

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-
+const routes: Routes = [
+  { path: 'home', component: AppComponent }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
+
+
+
+
