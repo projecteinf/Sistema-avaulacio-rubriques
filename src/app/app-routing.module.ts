7323105e14903ca97bd9c976e400277369060e5b
ProjecteInf <projecteinf@boscdelacoma.cat>
2022-08-08 14:14:09 +0000

    Implantació plana 404 Not Found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './projecte/components/home/home.component';
 import { LoginComponent } from './projecte/components/login/login.component';
-
+import { Error404Component } from './projecte/components/error404/error404.component';
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: '**', component: HomeComponent}
+  { path: '**', component: Error404Component}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { MaterialModule } from '../lib/material.module';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './projecte/components/home/home.component';
 import { LoginComponent } from './projecte/components/login/login.component';
+import { Error404Component } from './error404/error404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
+    Error404Component,
   ],
   imports: [
     MaterialModule,
