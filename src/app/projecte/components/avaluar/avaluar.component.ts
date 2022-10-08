bdb69d8bc81a66906542623579863d32dedcd6d6
ProjecteInf <projecteinf@boscdelacoma.cat>
2022-10-08 10:06:16 +0000

    Obtenir rubrica segons curs alumne - gestionar múltiples cursos

--- src/app/projecte/components/avaluar/avaluar.component.ts
+++ src/app/projecte/components/avaluar/avaluar.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Login } from '../../_model/02-entitiesLayer/entities/login/Login';
 import { LoginWebService } from '../../_model/01-serviceLayer/api/loginWebService';
 import { RubricaWebService } from '../../_model/01-serviceLayer/api/RubricaWebService';
-import { LoginDAO } from '../../../projecte/_model/03-persistenceLayer/impl/webStorage/daos/login/LoginDAO';
 import { WebStoragePersistenceManager } from '../../_model/03-persistenceLayer/managers/webStoragePersistenceManager';
 
 @Component({
@@ -15,6 +14,8 @@ export class AvaluarComponent {
   students: Login[] = new Array<Login>();
   cursos!: String[];
   rubrica: any;
+  selCurs: boolean = false;
+  currentCurs!: string;
   
   constructor(private loginWebService: LoginWebService,private rubricaWebService:RubricaWebService) { 
     this.getToken().subscribe(token => {
@@ -24,24 +25,20 @@ export class AvaluarComponent {
         WebStoragePersistenceManager.saveData(this.getTeacherName(token),JSON.stringify(this.students));
       });
     });
-
-    rubricaWebService.getRubrica("2DAW").subscribe(rubrica => {
-      this.rubrica = rubrica;
-    })
   }
-
-  getTeacherName(token:any) { return JSON.parse(token).name;}
-
-  cursing(curs:any, student:any) { 
-    return student.cursos.filter((cursant: any) => curs.includes(cursant)).length>0; 
-  }
-
-  getToken() { return this.loginWebService.getToken();  }
 
   studentChange(current:any) {
     this.getToken().subscribe(token => {
       let students = JSON.parse(WebStoragePersistenceManager.getData(this.getTeacherName(token)));
       let cursos = students.filter( (student: { user: any; })  =>  student.user==current.value.user )[0].cursos;
+      if (this.selCurs = cursos.length>1) {
+        this.cursos=cursos;
+        this.rubrica=null;
+      }
+      else {
+        this.currentCurs=cursos[0];
+        this.getRubrica(this.currentCurs);
+      }
     }
   );}
   
@@ -49,5 +46,17 @@ export class AvaluarComponent {
     const isValue = st1 && st2 ? st1.usuari == st2.usuari : st1 === st2;
     if (isValue) Object.assign(st2,st1);
     return isValue;
+  }
+
+  cursing(curs:any, student:any) { 
+    return student.cursos.filter((cursant: any) => curs.includes(cursant)).length>0; 
+  }
+
+  getToken() { return this.loginWebService.getToken();  }
+  getTeacherName(token:any) { return JSON.parse(token).name;}
+  getRubrica(curs:string) {
+    this.rubricaWebService.getRubrica(curs).subscribe(rubrica => {
+      this.rubrica = rubrica;
+    })
   }
 }
