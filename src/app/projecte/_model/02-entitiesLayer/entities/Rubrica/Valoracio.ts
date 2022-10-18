dc39a803e141354f469f59f79e47446ac3becf64
ProjecteInf <projecteinf@boscdelacoma.cat>
2022-10-18 09:34:42 +0000

    Marcar valoració seleccionada

--- src/app/projecte/_model/02-entitiesLayer/entities/Rubrica/Valoracio.ts
+++ src/app/projecte/_model/02-entitiesLayer/entities/Rubrica/Valoracio.ts
@@ -4,11 +4,13 @@ export class Valoracio extends EntityBase{
     nom: string;
     notaMaxima: number;
     notaMinima: number;
+    seleccionada: boolean;
 
     public constructor(nom:string,notaMaxima:number,notaMinima:number) {
         super();
         this.nom = nom;
         this.notaMaxima = notaMaxima;
         this.notaMinima = notaMinima;
+        this.seleccionada = false;
     }
 }
