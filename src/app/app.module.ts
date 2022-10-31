5a0f5db6b24d59258d0afb1df5eed490c462f3de
ProjecteInf <projecteinf@boscdelacoma.cat>
2022-10-31 11:14:04 +0000

    Generat component getStudents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { isTeacher } from './projecte/_model/01-serviceLayer/impl/AuthGuard/isTe
 import { isStudent } from './projecte/_model/01-serviceLayer/impl/AuthGuard/isStudent.service';
 import { PasswordsComponent } from './projecte/components/passwords/passwords.component';
 import { ChangePasswordComponent } from './projecte/components/change-password/change-password.component';
+import { GetStudentsComponent } from './get-students/get-students.component';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { ChangePasswordComponent } from './projecte/components/change-password/c
     AvaluarComponent,
     PasswordsComponent,
     ChangePasswordComponent,
+    GetStudentsComponent,
     
   ],
   imports: [
---
+++ src/app/get-students/get-students.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-get-students',
+  templateUrl: './get-students.component.html',
+  styleUrls: ['./get-students.component.css']
+})
+export class GetStudentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
